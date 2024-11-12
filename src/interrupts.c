b68f2d04efa1179f2ca2ce2f36fcfcbce32c4cd0
Navya Datla <ndatla@purdue.edu>
2024-11-12 16:22:28 -0500

    timers and interrupts

--- src/interrupts.c
+++ src/interrupts.c
@@ -6,18 +6,24 @@
 #include "ps2.h"
 
 //insert function definitions here
-int GAMETIME; // needs to be set to 15 when the game starts
+int GAMETIME = 15; // needs to be set to 15 when the game starts
 
 void TIM7_IRQHandler(){
   TIM7->SR &= ~TIM_SR_UIF;
   GAMETIME -= 1;
-
-    if(GAMETIME == 1){
-      //end of game function
-    }
+  write_display();
   }
 
 void write_display() {
-  snprintf(GAMETIME, sizeof(GAMETIME), "%d\n");
+  if(GAMETIME >= 0){
+    snprintf(GAMETIME, sizeof(GAMETIME), "%d\n");
+    return;
+  }  
+  else
+  {
+    snprintf(0, sizeof(GAMETIME), "%d\n");
+    return;
+  }
+
 }
 
