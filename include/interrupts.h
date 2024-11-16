ed8ab41af78b5ef8fa8f444cbf11517c12767ead
Aditi Sheela Akella <akella3@ecelnx59.ecn.purdue.edu>
2024-11-15 23:40:46 -0500

    function declarations

--- include/interrupts.h
+++ include/interrupts.h
@@ -7,5 +7,13 @@
 
 //INSERT FUNCTION DECLARATIONS HERE 
 void TIM7_IRQHandler(void);
+void write_display(void);
+void init_spi1(void);
+void spi_cmd(unsigned int data);
+void spi_data(unsigned int data);
+void spi1_init_oled(void);
+void spi1_display1(const char *string);
+void spi1_setup_dma(void);
+void spi1_enable_dma(void);
 
 #endif
--- include/timer.h
+++ include/timer.h
@@ -4,5 +4,6 @@
 //insert function declarations here
 
 void setup_tim7(void);
+void init_tim15(void);
 
 #endif
