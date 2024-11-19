b2ef1f133e1bada9203a3c1a88f5ff92b9ba53eb
Anh Kim Ho <ho219@ecelnx80.ecn.purdue.edu>
2024-11-18 19:39:24 -0500

    starting

--- src/ps2.c
+++ src/ps2.c
@@ -2,3 +2,8 @@
 #include "stm32f0xx.h"
 
 //insert function definitions here 
+void init_gpio (){
+    RCC -> AHBENR |= RCC_AHBENR_GPIOCEN;//enable clk for gpioc
+    GPIOC -> MODER &= ~(GPIO_MODER_MODER0 |GPIO_MODER_MODER1); //input for pc0,pc1
+}
+
