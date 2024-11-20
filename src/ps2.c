a04864984113c2e0074496d283f309b36eadb1ea
akh3121 <akh3121@gmail.com>
2024-11-19 23:39:12 -0500

    working on interrupts

--- src/ps2.c
+++ src/ps2.c
@@ -47,36 +47,48 @@ void setup_tim1(){
     GPIOA -> AFR[1] &= ~0xF;
     GPIOA -> AFR[1] |= 2; //setting af2 (tim1_ch1) for PA8
     //PA8-> clock, PA0-> data 
-    // TIM1 -> CR2 &= ~TIM_CR2_TI1S;//setting tim1_ch1 to ti1 input
-    TIM1 -> CCMR1 &= ~TIM_CCMR1_CC1S_1; //CC1S:01 for input , mapped to TI1
-    TIM1 -> CCMR1 |= TIM_CCMR1_CC1S_0;
-    TIM1 -> PSC = 10 -1;//480kHz
-    TIM1 -> ARR = 10-1; 
-    TIM1 -> SMCR &= ~TIM_SMCR_TS; //trigger selection for internal trigger
+    // TIM1 -> CCMR1 &= ~TIM_CCMR1_CC1S_1; //CC1S:01 for input , mapped to TI1
+    // TIM1 -> CCMR1 |= TIM_CCMR1_CC1S_0;
+    // TIM1 -> PSC = 10 -1;//480kHz
+    // TIM1 -> ARR = 10-1; 
+    // TIM1 -> SMCR &= ~TIM_SMCR_TS; //trigger selection for internal trigger
+    // TIM1 -> CCMR1 &= ~TIM_CCMR1_IC1F;//clearing filter
+    // TIM1 -> CCMR1 |= TIM_CCMR1_IC1F_1;//setting filter with 4 clock cycles
+    // TIM1 -> CCER &= ~TIM_CCER_CC1NP; //CC1NP/CC1P = 01 for falling edge
+    // TIM1 -> CCER |= TIM_CCER_CC1P;
+    // TIM1 -> CCMR1 &= ~(TIM_CCMR1_IC1PSC); //enable capture at each valid transition
+    // TIM1 -> CCER |= TIM_CCER_CC1E; //enable capture from counter into the capture register
+    // TIM1 -> DIER |= TIM_DIER_CC1IE; //enable interrupt
+    // NVIC -> ISER[0] = 1 <<  TIM1_CC_IRQn;
+    // TIM1 -> CR1 |= TIM_CR1_CEN;
 //     // //IDK how to configure the tim1 to tim1_ch1 or if that's even needed
 //     // //Ask about using a timer in capture mode to trigger an interrupt on the falling edge of a gpio pin
 //     // //Also need to find the input filter duration, PS-2, takes 5-25 microseconds to transition from low to high/vice versa
-    TIM1 -> CCMR1 &= ~TIM_CCMR1_IC1F;//clearing filter
-    TIM1 -> CCMR1 |= TIM_CCMR1_IC1F_1;//setting filter with 4 clock cycles
-    TIM1 -> CCER &= ~TIM_CCER_CC1NP; //CC1NP/CC1P = 01 for falling edge
-    TIM1 -> CCER |= TIM_CCER_CC1NP | TIM_CCER_CC1P;
-    TIM1 -> CCMR1 &= ~(TIM_CCMR1_IC1PSC); //enable capture at each valid transition
+    TIM1 -> CR1 &= ~TIM_CR1_CEN;//disable
+    TIM1 -> CCMR1 &= ~TIM_CCMR1_CC1S; //CC1S: 01, input, TI1
+    TIM1 -> CCMR1 |= TIM_CCMR1_CC1S_0;
+    TIM1 -> CCER &= ~TIM_CCER_CC1NP;
+    TIM1 -> CCER |= TIM_CCER_CC1P;//setting trigger to falling edge: CC1NP/CC1P = 01
     TIM1 -> CCER |= TIM_CCER_CC1E; //enable capture from counter into the capture register
+    
+    TIM1 -> PSC = 10 -1;//480kHz
+    TIM1 -> ARR = 10-1;
+    TIM1 -> SMCR &= ~TIM_SMCR_SMS;
+    TIM1 -> SMCR |= TIM_SMCR_TS; //setting TS: 111 External trigger input
+    TIM1 -> SMCR &= ~TIM_SMCR_ETF;
+    TIM1 -> SMCR |= TIM_SMCR_ETF_1;//setting external trigger filter to 0010: 4 cycles
     TIM1 -> DIER |= TIM_DIER_CC1IE; //enable interrupt
     NVIC -> ISER[0] = 1 <<  TIM1_CC_IRQn;
-    TIM1 -> CR1 |= TIM_CR1_CEN;
-    // TIM1->CCMR1 |= TIM_CCMR1_CC1S_0| TIM_CCMR1_IC1F_0 | TIM_CCMR1_IC1F_1; /* (1)*/
-    // TIM1->CCER |= TIM_CCER_CC1E; /* (2) */
-    // TIM1->DIER |= TIM_DIER_CC1IE; /* (3) */
-    // NVIC -> ISER[0] = 1 <<  TIM1_CC_IRQn;
-    // TIM1->CR1 |= TIM_CR1_CEN;
+    TIM1 -> CR1 |= TIM_CR1_CEN;//enable
+
+   
 }
 
 void TIM1_CC_IRQHandler(){
-    TIM1 -> SR &= ~ TIM_SR_UIF;
+    TIM1 -> SR &= ~ TIM_SR_CC1IF; //clearing interrupt flag
     //get data?
     ind += 1;
     //printf("hi");
-    msg = (GPIOA -> IDR & 0b1);//input of PA0
+    msg = (msg << 1) | (GPIOA -> IDR & 0b1);//input of PA0
 }
 
